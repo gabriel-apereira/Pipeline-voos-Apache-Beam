9c764aaa8005be77113d7a8e2eca42452c76c627
Gabriel Pereira <gabriel_assis66@hotmail.com>
2025-09-16 10:06:28 -0300

    Add files via upload

---
+++ notebooks/Voos Batch Dataflow + GCS.py
@@ -0,0 +1,51 @@
+import apache_beam as beam
+import os
+from apache_beam.options.pipeline_options import PipelineOptions
+
+serviceAccount = r'C:\Users\gabri\Desktop\Udemy\Apache GCP\dataflow-beam-voos-b1227139fe6e.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = serviceAccount
+
+pipeline_options = {
+    'project': 'dataflow-beam-voos',
+    'runner': 'DataflowRunner',
+    'region': 'southamerica-east1',
+    'staging_location': 'gs://apache_beam_voos/temp',
+    'temp_location': 'gs://apache_beam_voos/temp',
+    'template_location': 'gs://apache_beam_voos/template/batch_job_df_gcs_voos'}
+
+pipeline_options = PipelineOptions.from_dictionary(pipeline_options)
+p1 = beam.Pipeline(options=pipeline_options)
+
+
+class filtro(beam.DoFn):
+    def process(self, record):
+        if int(record[8]) > 0:
+            return [record]
+
+
+Tempo_Atrasos = (
+    p1
+    | "Importar Dados Atraso" >> beam.io.ReadFromText(r"gs://apache_beam_voos/entrada/voos_sample.csv", skip_header_lines=1)
+    | "Separar por Vírgulas Atraso" >> beam.Map(lambda record: record.split(','))
+    | "Pegar voos com atraso" >> beam.ParDo(filtro())
+    | "Criar par atraso" >> beam.Map(lambda record: (record[4], int(record[8])))
+    | "Somar por key" >> beam.CombinePerKey(sum)
+)
+
+
+Qtd_Atrasos = (
+    p1
+    | "Importar Dados" >> beam.io.ReadFromText(r"gs://apache_beam_voos/entrada/voos_sample.csv", skip_header_lines=1)
+    | "Separar por Vírgulas Qtd" >> beam.Map(lambda record: record.split(','))
+    | "Pegar voos com Qtd" >> beam.ParDo(filtro())
+    | "Criar par Qtd" >> beam.Map(lambda record: (record[4], int(record[8])))
+    | "Contar por key" >> beam.combiners.Count.PerKey()
+)
+
+tabela_atrasos = (
+    {'Qtd_Atrasos': Qtd_Atrasos, 'Tempo_Atrasos': Tempo_Atrasos}
+    | "Group By" >> beam.CoGroupByKey()
+    | "Saida Para GCP" >> beam.io.WriteToText(r"gs://apache_beam_voos/saida/Voos_atrados_qtd.csv")
+)
+
+result = p1.run()
